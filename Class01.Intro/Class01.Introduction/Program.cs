4f017f1a28204b560a0a2fb8f4e18b0f7313952f
vikt0rija99 <121236336+vikt0rija99@users.noreply.github.com>
2023-03-16 12:25:50 +0100

    Add files via upload

---
+++ Class01.Intro/Class01.Introduction/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("    *    ");
+Console.WriteLine("   ***   ");
+Console.WriteLine("  *****  ");
+Console.WriteLine(" ******* ");
+Console.WriteLine("*********");
+
+
+
+Console.WriteLine("    *    ");
+Console.WriteLine("    *    ");
+Console.WriteLine("  * * *  ");
+Console.WriteLine(" *  *  * ");
+Console.WriteLine("    *    ");
+Console.WriteLine("    *    ");
+Console.WriteLine("  * * *  ");
+Console.WriteLine(" *     * ");
+
+
+
