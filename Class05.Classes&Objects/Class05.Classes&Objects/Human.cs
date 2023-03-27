992101dc9e8c9148d04ad97d790057b76d80a049
vikt0rija99 <121236336+vikt0rija99@users.noreply.github.com>
2023-03-27 09:34:57 +0200

    Add files via upload

---
+++ Class05.Classes&Objects/Class05.Classes&Objects/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class05.Classes_Objects
+{
+    public class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public string GetPersonalDetails()
+        {
+            return $"{FirstName} {LastName} is {Age} years old";
+        }
+    }
+}
---
+++ Class05.Classes&Objects/Class05.Classes&Objects/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Class05.Classes_Objects;
+
+Console.WriteLine("Hello, World!");
+
+//Task 1
+//Create a class Human
+//Add properties: FirstName, LastName, Age
+//Create a method called GetPersonDetails that returns the full name of the human as well as their age
+//Create an object human by asking the user to fill the required information (take first name, last name and age from user input)
+//Call the GetPersonDetails method and print the result in the console after the object is created
+
+Human human = new Human();
+
+Console.WriteLine("Enter firstname: ");
+human.FirstName = Console.ReadLine();
+
+Console.WriteLine("Enter lastname: ");
+human.LastName = Console.ReadLine();
+
+Console.WriteLine("Enter age: ");
+human.Age  = int.Parse(Console.ReadLine());
+
+string humanDetails = human.GetPersonalDetails();
+Console.WriteLine(humanDetails);
